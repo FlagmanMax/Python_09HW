b663b0022e6f9b3f4ef532e0c6d254e2d38438dd
FlagmanMax <borodov.m@gmail.com>
2022-12-29 01:18:04 +0300

    token added

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import command_hi, command_time, command_help, command_sum
 
 
-app = ApplicationBuilder().token("ABC").build()
+app = ApplicationBuilder().token("5876810231:AAENQkudk1Tl9pTicU7wKl8atSEFLH3heds").build()
 
 app.add_handler(CommandHandler("hi", command_hi))
 app.add_handler(CommandHandler("time", command_time))
